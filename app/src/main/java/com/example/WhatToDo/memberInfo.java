3c720377e7a13c1133e33c48896fc51c1f020b79
damichaels <damichaels@crimson.ua.edu>
2020-02-25 11:20:08 -0600

    taskSchedule, memberInfo, groupMembers

---
+++ app/src/main/java/com/example/WhatToDo/memberInfo.java
@@ -0,0 +1,15 @@
+package com.example.WhatToDo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class memberInfo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
