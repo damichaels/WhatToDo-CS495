7cf26fee7ac4d565b9096401236d9ba42666148c
damichaels <damichaels@crimson.ua.edu>
2020-02-25 12:10:01 -0600

    working xml memberInfo,groupMembers,task,Schedule

--- app/src/main/java/com/example/WhatToDo/taskSchedule.java
+++ app/src/main/java/com/example/WhatToDo/taskSchedule.java
@@ -10,7 +10,7 @@ public class taskSchedule extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_task_schedule);
     }
 
 }
