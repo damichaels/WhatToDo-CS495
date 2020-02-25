5ea81973b5180e1fe4cd77f68c4af093cd50a927
JohnMatheson97 <48697852+JohnMatheson97@users.noreply.github.com>
2020-02-24 20:47:40 -0600

    Add files via upload

---
+++ app/src/main/res/layout/GroupList.java
@@ -0,0 +1,40 @@
+package com.example.hellospring2020;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class GroupList extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<String> groupNames = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.group_list);
+        ArrayList<GroupName> groupList = new ArrayList<>();
+        groupList.add(new GroupName("Smith Family Chores"));
+        groupList.add(new GroupName("Bama Roommates"));
+        groupList.add(new GroupName("The Office"));
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new GroupListAdapter(groupList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+            }
+
+    }
+
+
+
+
+
